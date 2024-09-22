fd20d20fb0598a351b59324daa66cf0d1d7e6bac
Sikindar <mdimran1861999@gmail.com>
2024-09-22 09:48:18 +0530

    Converted to PageObjectModel

---
+++ src/Pages/UserLogin.java
@@ -0,0 +1,33 @@
+package Pages;
+
+import org.openqa.selenium.InvalidSelectorException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class UserLogin {
+	WebDriver driver;
+
+	public UserLogin(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "(//a[@title='Login']/parent::div)[2]")
+	WebElement login;
+
+	@FindBy(xpath = "//span[text()='Enter Email/Mobile number']/parent::label/parent::div/input")
+	WebElement enterUserName;
+
+	@FindBy(xpath = "//button[contains(text(),'OTP')]")
+	WebElement requestOTP;
+
+	public void applicationLogin(String number) {
+		login.click();
+		System.out.println("Error");
+		enterUserName.sendKeys(number);
+		requestOTP.click();
+	}
+
+}
--- src/Test/Launch_Application.java
+++ src/Test/Launch_Application.java
@@ -1,11 +1,18 @@
+package Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import Pages.UserLogin;
+
 public class Launch_Application {
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Downloads\\chromedriver-win64\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\DELL\\Downloads\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.flipkart.com/");
 		driver.manage().window().maximize();
+		UserLogin launch = new UserLogin(driver); 
+		launch.applicationLogin("9949923313");
 	}
 }
